fd505af6ed94011abf5ea2f2fa987022aff0a0dd
Nicolette <colettenikiforov@gmail.com>
2026-05-14 21:22:49 +0800

    Questions FontSize fix

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -214,7 +214,7 @@ const QuestionPanel = styled(Box)({
 
 const QuestionText = styled(Typography)({
   fontFamily: "'Press Start 2P', 'Courier New', monospace",
-  fontSize: "15px",
+  fontSize: "20px",
   color: "#35E52B",
   textShadow: "0 0 8px #42FF5C, 0 0 20px #35E52B55",
   textAlign: "center",
@@ -281,7 +281,7 @@ const AnswerButton = styled(Button, {
 
   return {
     fontFamily: "'Press Start 2P', 'Courier New', monospace",
-    fontSize: "13px",
+    fontSize: "20px",
     width: "100%",
     height: "100%",
     padding: "0 20px",
@@ -337,10 +337,10 @@ const AnswerButton = styled(Button, {
 
 const AnswerLabel = styled(Box, {
   shouldForwardProp: (prop) => prop !== 'correct' && prop !== 'incorrect',
-})<{ correct?: boolean; incorrect?: boolean }>(
+})<{ correct?: boolean; incorrect?: boolean }>( 
   ({ correct, incorrect }) => ({
     fontFamily: "'Press Start 2P', 'Courier New', monospace",
-    fontSize: "13px",
+    fontSize: "18px",
     color: correct ? "#35E52B" : incorrect ? "#E33232" : "#00E5FF",
     textShadow: correct
       ? "0 0 8px #35E52B"
@@ -978,7 +978,7 @@ useEffect(() => {
           <Typography
             sx={{
               fontFamily: "'Press Start 2P', monospace",
-              fontSize: "20px",
+              fontSize: "23px",
               color: "#35E52B",
               textShadow: "0 0 8px #42FF5C",
               letterSpacing: "3px",
