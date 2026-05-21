81fbc8e73ef1f55ec46d3f10a82de3836ee92c99
Loel Joseph Hofileña <loeljoseph.hofilena@students.isatu.edu.ph>
2026-05-22 00:29:08 +0800

    cherry-pick(refactor/stuff): update asset paths (forge.config icon path)

--- forge.config.ts
+++ forge.config.ts
@@ -7,10 +7,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 import MakerPortable from '@rabbitholesyndrome/electron-forge-maker-portable';
 const config: ForgeConfig = {
   packagerConfig: {
-<<<<<<< HEAD
     icon: 'public/img/app-icon.ico',
-=======
->>>>>>> 26a5ef6 (update asset paths)
     asar: {
       unpack: "**/*.{png,jpg,jpeg,gif,webp,mp3,ttf,json}",
     },
